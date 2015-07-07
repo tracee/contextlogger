7e69d6042b631ba63053921b01a0f75fac2cced9
Tobias Gindler <tobias.gindler@holisticon.de>
2015-07-07 10:21:48 +0200

    Altered initialization method to Test to clear MDC.

--- integration-test/general/src/test/java/io/tracee/contextlogger/integrationtest/ImplicitContextIntegrationTest.java
+++ integration-test/general/src/test/java/io/tracee/contextlogger/integrationtest/ImplicitContextIntegrationTest.java
@@ -1,49 +1,57 @@
 package io.tracee.contextlogger.integrationtest;
 
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
-import org.junit.Test;
-
 import io.tracee.contextlogger.TraceeContextLogger;
 import io.tracee.contextlogger.contextprovider.api.ImplicitContext;
 import io.tracee.contextlogger.contextprovider.api.Profile;
 import io.tracee.contextlogger.outputgenerator.writer.BasicOutputWriterConfiguration;
 import io.tracee.contextlogger.util.test.RegexMatcher;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.MDC;
 
 /**
  * Integration test for {@link ImplicitContext} wrapping.
  */
 public class ImplicitContextIntegrationTest {
 
-    @Test
-    public void shouldOutputImplicitContextCorrectly() {
-        String result = TraceeContextLogger.create().enforceOutputWriterConfiguration(BasicOutputWriterConfiguration.JSON_INLINE)
-                .enforceProfile(Profile.BASIC).apply().toString(ImplicitContext.COMMON, ImplicitContext.TRACEE);
+	@Before
+	public void init() {
+		// clean MDC
+		MDC.clear();
+	}
 
-        MatcherAssert.assertThat(result, RegexMatcher.matches("\\[\"TYPE:Object\\[]\",\\{\"TYPE\":\"common\".*"));
-    }
+	@Test
+	public void shouldOutputImplicitContextCorrectly() {
+		String result = TraceeContextLogger.create().enforceOutputWriterConfiguration(BasicOutputWriterConfiguration.JSON_INLINE)
+				.enforceProfile(Profile.BASIC).apply().toString(ImplicitContext.COMMON, ImplicitContext.TRACEE);
 
-    @Test
-    public void shouldOutputSingleEmptyImplicitContextCorrectly() {
+		MatcherAssert.assertThat(result, RegexMatcher.matches("\\[\"TYPE:Object\\[]\",\\{\"TYPE\":\"common\".*"));
+	}
 
-        String result = TraceeContextLogger.create().enforceOutputWriterConfiguration(BasicOutputWriterConfiguration.JSON_INLINE)
-                .enforceProfile(Profile.BASIC).apply().toString(ImplicitContext.TRACEE);
+	@Test
+	public void shouldOutputSingleEmptyImplicitContextCorrectly() {
 
-        MatcherAssert.assertThat(result, Matchers.is("{\"TYPE\":\"tracee\",\"DYNAMIC\":null}"));
-    }
 
-    @Test
-    public void should_write_instance_for_multiple_referenced_instances() {
-        String result = TraceeContextLogger.create().enforceProfile(Profile.BASIC).apply().toString("ABC");
+		String result = TraceeContextLogger.create().enforceOutputWriterConfiguration(BasicOutputWriterConfiguration.JSON_INLINE)
+				.enforceProfile(Profile.BASIC).apply().toString(ImplicitContext.TRACEE);
 
-        MatcherAssert.assertThat(result, Matchers.is("\"String['ABC']\""));
-    }
+		MatcherAssert.assertThat(result, Matchers.is("{\"TYPE\":\"tracee\",\"DYNAMIC\":null}"));
+	}
 
-    @Test
-    public void should_write_this_instance_without_tostring_overwrite_correctly() {
-        String result = TraceeContextLogger.create().enforceProfile(Profile.BASIC).apply().toString(this);
+	@Test
+	public void should_write_instance_for_multiple_referenced_instances() {
+		String result = TraceeContextLogger.create().enforceProfile(Profile.BASIC).apply().toString("ABC");
 
-        MatcherAssert.assertThat(result, Matchers.is("\"ImplicitContextIntegrationTest[]\""));
-    }
+		MatcherAssert.assertThat(result, Matchers.is("\"String['ABC']\""));
+	}
+
+	@Test
+	public void should_write_this_instance_without_tostring_overwrite_correctly() {
+		String result = TraceeContextLogger.create().enforceProfile(Profile.BASIC).apply().toString(this);
+
+		MatcherAssert.assertThat(result, Matchers.is("\"ImplicitContextIntegrationTest[]\""));
+	}
 
 }
