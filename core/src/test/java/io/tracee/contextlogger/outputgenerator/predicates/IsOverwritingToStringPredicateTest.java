22e3426597fdbc9305c4d7886bfb3526e25dd957
Tobias Gindler <tobias.gindler@holisticon.de>
2015-10-01 17:02:34 +0200

    Use toString method of superclasses if possible. (except Objects toString method)

--- core/src/test/java/io/tracee/contextlogger/outputgenerator/predicates/IsOverwritingToStringPredicateTest.java
+++ core/src/test/java/io/tracee/contextlogger/outputgenerator/predicates/IsOverwritingToStringPredicateTest.java
@@ -1,28 +1,45 @@
 package io.tracee.contextlogger.outputgenerator.predicates;
 
+import io.tracee.contextlogger.outputgenerator.writer.TestOutputElementWriter;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.Test;
-
-import io.tracee.contextlogger.outputgenerator.writer.TestOutputElementWriter;
 
 /**
  * Test class for {@link IsOverwritingToStringPredicate}.
  */
 public class IsOverwritingToStringPredicateTest {
 
-    @Test
-    public void should_detect_overwritten_tostring_method_correctly() {
-        MatcherAssert.assertThat(IsOverwritingToStringPredicate.getInstance().apply(this), Matchers.is(true));
-    }
+	public static class TestClassWithToStringInSuperclass extends IsOverwritingToStringPredicateTest {
 
-    @Test
-    public void should_not_detect_non_overwritten_tostring_method() {
-        MatcherAssert.assertThat(IsOverwritingToStringPredicate.getInstance().apply(new TestOutputElementWriter()), Matchers.is(false));
-    }
+	}
 
-    public String toString() {
-        return "MURX";
-    }
+	public static class TestClassWithDefaultToString {
+
+	}
+
+	@Test
+	public void should_detect_overwritten_tostring_method_correctly() {
+		MatcherAssert.assertThat(IsOverwritingToStringPredicate.getInstance().apply(this), Matchers.is(true));
+	}
+
+	@Test
+	public void should_not_detect_non_overwritten_tostring_method1() {
+		MatcherAssert.assertThat(IsOverwritingToStringPredicate.getInstance().apply(new TestOutputElementWriter()), Matchers.is(false));
+	}
+
+	@Test
+	public void should_not_detect_non_overwritten_tostring_method2() {
+		MatcherAssert.assertThat(IsOverwritingToStringPredicate.getInstance().apply(new TestClassWithDefaultToString()), Matchers.is(false));
+	}
+
+	@Test
+	public void should_detect_non_overwritten_tostring_method_correctly() {
+		MatcherAssert.assertThat(IsOverwritingToStringPredicate.getInstance().apply(new TestClassWithToStringInSuperclass()), Matchers.is(true));
+	}
+
+	public String toString() {
+		return "MURX";
+	}
 
 }
